db65e96922cc500ba216e171d6eed0e8ca8cdf2d
nishithak02 <nishitha.kommineni@slu.edu>
2024-11-17 07:34:36 +0000

    completed with lab 04

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs').promises
-const path = require('path')
 const express = require('express')
+const api = require('./api')
+const middleware = require('./middleware')
+const bodyParser = require('body-parser')
 
 // Set the port
 const port = process.env.PORT || 3000
@@ -8,9 +9,18 @@ const port = process.env.PORT || 3000
 const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
+app.use(middleware.cors)
+app.use(middleware.cors)
+app.use(bodyParser.json())
 // register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
+app.post('/products', api.createProduct)
+app.delete('/products/:id', api.deleteProduct); 
+app.put('/products/:id', api.updateProduct);    
+app.use(middleware.handleError)
+app.use(middleware.notFound)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
